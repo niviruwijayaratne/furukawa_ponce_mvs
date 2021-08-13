52c9c3ceb617d77739a4fec0c733b359e36626d9
niviruwijayaratne <nnw2@illinois.edu>
2021-08-13 01:46:44 -0700

    fundamental matrix calculation, epipolar correspondences implemented + helper functions for reading camera params and reshaping filtered responses

---
+++ helpers.py
@@ -0,0 +1,63 @@
+import os
+import numpy as np
+import cv2
+
+def read_camera_params(image_dir):
+    files = os.listdir(image_dir)
+    camera_params = None
+    lat_lon_params = None
+    for file in files:
+        if file.endswith('par.txt'):
+            camera_params = os.path.join(image_dir, file)
+        if file.endswith('ang.txt'):
+            lat_lon_params = os.path.join(image_dir, file)
+
+    with open(camera_params, 'r') as f:
+        camera_params = f.readlines()[1:]
+    
+    with open(lat_lon_params, 'r') as f:
+        lat_lon_params = f.readlines()
+    
+    cam_matrices = []
+    for i, l in enumerate(camera_params):
+        cam_matrices.append({})
+        line = l.split(" ")
+        lat_lon = lat_lon_params[i].split(" ")
+        K = np.array(line[1:10]).reshape(3, 3).astype(np.float32)
+        R = np.array(line[10:19]).reshape(3, 3).astype(np.float32)
+        T = np.array(line[19:]).reshape(3, 1).astype(np.float32)
+
+        cam_matrices[i]['K'] = K
+        cam_matrices[i]['R'] = R
+        cam_matrices[i]['T'] = T
+        cam_matrices[i]['P'] = K.dot(np.hstack([R, T]))
+        print(float(lat_lon[0]) > 0)
+        cam_matrices[i]['rot_angle'] = cv2.ROTATE_90_COUNTERCLOCKWISE if float(lat_lon[0]) < 0 else cv2.ROTATE_90_CLOCKWISE
+    return cam_matrices
+
+def skew_symmetric(vec):
+    mat = np.zeros((3, 3))
+    mat[0, 1] = -vec[2]
+    mat[1, 0] = vec[2]
+
+    mat[0, 2] = vec[1]
+    mat[2, 0] = -vec[1]
+
+    mat[1, 2] = -vec[0]
+    mat[2, 1] = vec[0]
+
+    return mat
+
+def reshape_max_responses(features):
+    dims = (features.shape[0], features.shape[1]*features.shape[2])
+    reshaped_features = np.zeros(dims)
+    reshaped_features[:, 0::2] = features[:, 0]
+    reshaped_features[:, 1::2] = features[:, 1]
+    reshaped_features = reshaped_features.reshape(-1, 2).astype(np.int32)
+    reshaped_features = np.flip(reshaped_features, axis=1)
+
+    return reshaped_features
+
+    
+    
+        
--- matching.py
+++ matching.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
 import os
+import helpers as hlp
+import sys
+
+IMAGE_CLASS = 'temple'
+IMAGE_DIR = os.path.join("data", IMAGE_CLASS)
 
 class Matcher:
     def __init__(self):
@@ -8,6 +13,7 @@ class Matcher:
         self.sigma = 1
         self.beta2 = 32
         self.eta = 4
+        self.camera_params = hlp.read_camera_params(IMAGE_DIR)
 
     def construct_gaussian(self, ksize: int, sigma: int = 1):
         '''
@@ -59,20 +65,24 @@ class Matcher:
     
         dog = np.zeros_like(im)
         im = np.pad(im, ((pad, pad), (pad, pad)), mode='constant', constant_values=0)
+
         for y in range(0, im.shape[0] - ksize):
             for x in range(0, im.shape[1] - ksize):
                 window = im[y: y + ksize, x: x + ksize]
                 convolved = np.sum(np.multiply(window, G))
                 dog[y, x] = convolved
+
         return dog
         
     def filter_responses(self, im: np.ndarray, harris: np.ndarray, dog: np.ndarray, visualize:bool = False) -> dict:
         '''
-        Returns dictionary: keys = ['harris', 'dog'], values = coordinates of respective detected features
+        Returns dictionary: keys = ['harris', 'dog'], values = coordinates (x, y) of respective detected features
             Input: H x W x N original image, H x W x 1 harris feature image, H x W x 1 dog feature image, 
             optional bool for visualizing detections
             Output: (H/self.beta2)*(W/self.beta2) x 2 x self.eta, dict[key] 
         '''
+
+
         max_responses = {'harris': [], 'dog': []}
         for y in range(0, im.shape[0], self.beta2):
             for x in range(0, im.shape[1], self.beta2):
@@ -83,27 +93,119 @@ class Matcher:
                 global_coords = np.array([y, x]).reshape(-1, 1)
                 dims = np.array([dimy, dimx]).reshape(-1, 1)
 
-                harris_window = harris[y: y + dimy, x: x + dimx].flatten()
-                max_harris = np.apply_along_axis(get_coords, 0, get_max(harris_window), global_coords, dims)
-                dog_window = dog[y: y + dimy, x: x + dimx].flatten()
-                max_dog = np.apply_along_axis(get_coords, 0, get_max(dog_window), global_coords, dims)
+                if not harris is None:
+                    harris_window = harris[y: y + dimy, x: x + dimx].flatten()
+                    max_harris = np.apply_along_axis(get_coords, 0, get_max(harris_window), global_coords, dims)
+                if not dog is None:
+                    dog_window = dog[y: y + dimy, x: x + dimx].flatten()
+                    max_dog = np.apply_along_axis(get_coords, 0, get_max(dog_window), global_coords, dims)
                 
                 max_responses['harris'].append(max_harris)
                 max_responses['dog'].append(max_dog)
                 if visualize:
                     cv2.rectangle(im, (x, y), (x + dimx, y + dimy), (0, 255, 0), 1)
-                    for yc, xc in zip(max_dog[0], max_dog[1]):
-                        cv2.circle(im, (xc, yc), 2, [255, 0, 0], -1)
-                    
-                    for yc, xc in zip(max_harris[0], max_harris[1]):
-                        cv2.circle(im, (xc, yc), 2, [0, 0, 255], -1)
+                    if not dog is None:
+                        for yc, xc in zip(max_dog[0], max_dog[1]):
+                            cv2.circle(im, (xc, yc), 2, [255, 0, 0], -1)
+                    if not harris is None:
+                        for yc, xc in zip(max_harris[0], max_harris[1]):
+                            cv2.circle(im, (xc, yc), 2, [0, 0, 255], -1)
         if visualize:
-            cv2.imshow('h', harris)
-            cv2.imshow('d', dog)
-            cv2.imshow('i', im)
+            cv2.imshow('harris', harris)
+            cv2.imshow('dog', dog)
+            cv2.imshow('original', im)
             cv2.waitKey(0)
         
-        max_responses['harris'] = np.array(max_responses['harris'])
-        max_responses['dog'] = np.array(max_responses['dog'])
+        if not harris is None:
+            max_harris_responses = np.array(max_responses['harris'])
+            max_responses['harris'] = hlp.reshape_max_responses(max_harris_responses) #x, y
+
+        if not dog is None:
+            max_dog_responses = np.array(max_responses['dog'])
+            max_responses['dog'] = hlp.reshape_max_responses(max_dog_responses) #x, y
 
         return max_responses
+
+    def get_fundamental_matrix(self, image_indices):
+        '''
+        Returns fundamental matrix between image_indices using given camera matrices
+        '''
+        ims = os.listdir(IMAGE_DIR)
+        ims = sorted(list(filter(lambda x: x.endswith(".png"), ims)))
+        im1, im2 = cv2.imread(os.path.join(IMAGE_DIR, ims[image_indices[0]])), cv2.imread(os.path.join(IMAGE_DIR, ims[image_indices[1]]))
+        im1 = cv2.rotate(im1, self.camera_params[image_indices[0]]['rot_angle'])
+        im2 = cv2.rotate(im1, self.camera_params[image_indices[1]]['rot_angle'])
+
+        P1 = self.camera_params[image_indices[0]]['P']
+        P2 = self.camera_params[image_indices[1]]['P']
+
+        U, S, V_t = np.linalg.svd(P1)
+        c = V_t[-1]
+        ep = P2.dot(c)
+        F = hlp.skew_symmetric(ep).dot(P2).dot(np.linalg.pinv(P1))
+
+        return F
+
+    def ssd(self, feature, win_size, im1, im2, get_y):
+        feature = [int(feature[0]), int(feature[1])]
+        ssd_window = im1[feature[0] - win_size//2: feature[0] + win_size//2 + 1, feature[1] - win_size//2: feature[1] + win_size//2 + 1]
+        min_ssd = float('inf')
+        corresp_feature = None
+        slope = (get_y(100) - get_y(0))/100
+        if slope < 0:
+            bounds = range(im1.shape[1], 1, -1)
+            get_next = -1
+        else:
+            bounds = range(im1.shape[1])
+            get_next = 1
+        for x in bounds:
+            y1 = int(get_y(x))
+            if y1 < 0 or y1 >= im2.shape[0]:
+                continue
+            else:
+                y2 = min(int(get_y(x + get_next)), im2.shape[0])
+                for y in range(y1, y2):
+                    window = im1[y - win_size//2: y + win_size//2 + 1, x - win_size//2: x + win_size//2 + 1]
+                    has_neg = np.array([y - win_size//2, y + win_size//2, x - win_size//2, x + win_size//2])
+                    if window.shape != ssd_window.shape or not (has_neg > 0).all():
+                        continue
+                    ssd = np.sum(np.square(ssd_window - window))
+                    if ssd < min_ssd:
+                        min_ssd = ssd
+                        corresp_feature = [y, x]
+
+        return corresp_feature
+
+    def epipolar_correspondence(self, point, base_im_idx):
+        '''
+        Given a feature (either harris or dog) in image with index base_im_idx, returns dictionary 
+        of features (of same type of given feature): key = image index, value = all features
+        that lie within 2 pixels of corresponding epipolar line
+        '''
+        ims = os.listdir(IMAGE_DIR)
+        ims = sorted(list(filter(lambda x: x.endswith(".png"), ims)))
+        base_im = cv2.rotate(cv2.imread(os.path.join(IMAGE_DIR, ims[base_im_idx])), self.camera_params[base_im_idx]['rot_angle'])
+        num_ims = len(ims)
+        other_im_idxs = list(range(0, base_im_idx))
+        other_im_idxs.extend(list(range(base_im_idx + 1, num_ims)))
+
+        features = {}
+        for idx in other_im_idxs[-2: ]:
+            im = cv2.rotate(cv2.imread(os.path.join(IMAGE_DIR, ims[idx])), self.camera_params[idx]['rot_angle'])
+            harris_responses = self.get_harris_response(im)
+            dog_responses = self.get_dog_response(im)
+            max_responses = self.filter_responses(im, harris_responses, dog_responses, False)
+            harris_points = np.hstack([max_responses['harris'], np.ones_like(max_responses['harris'][:, 0]).reshape(-1, 1)]).reshape(-1, 3)
+            F = self.get_fundamental_matrix([base_im_idx, idx])
+            homog_point = np.squeeze(np.vstack([np.array(point).reshape(-1, 1), [1]])) 
+            l = F.dot(homog_point).reshape(-1, 1)
+            distances = harris_points.dot(l)/(np.linalg.norm(harris_points[:, :-1], axis=1).reshape(-1, 1))
+            epipolar_consistent = harris_points[:, :-1][np.where(np.abs(distances) <= 2)[0]] #x, y
+            features[idx] = [epipolar_consistent]
+        
+        return features # x, y
+
+
+
+
+
